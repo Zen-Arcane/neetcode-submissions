234e9ff8c44dac5ec077c93010de8f169e56a0fb
Zen-Arcane <swaroopkrishnakr@gmail.com>
2026-08-08 19:54:55 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        l={}
+
+        if len(s) != len(t):
+            return False
+
+        for i in s:
+            l[i] = l.get(i,0)+1
+        
+        for j in t:
+            if j not in l:
+                return False
+            
+            l[j] = l.get(j,0)-1
+
+            if l[j] < 0:
+                return False
+        return True
+        
+
+
+
+        
